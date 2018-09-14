d191908f5b80fec118190bbbb539a5fe67d4e059
gurezo <akihiko.kigure@gmail.com>
2018-09-14 13:10:21 +0900

    temporary commit

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+  styleUrls: ['./page.component.scss'],
+  animations: [
+    trigger('popUp', [
+      state('true', style({ transform: 'translateY(0)'})),
+      transition('void => *', [
+        style({ transform: 'translateY(-20px)',
+                opacity: '0', }),
+        animate('500ms ease-out')
+      ])
+    ])
+  ]
+
 })
 export class PageComponent implements OnInit {
 
