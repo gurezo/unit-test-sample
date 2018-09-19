2079c8f71b027764c1b63f6ee02920f6796403c8
gurezo <akihiko.kigure@gmail.com>
2018-09-19 09:09:48 +0900

    no need code remove

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,22 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss'],
-  animations: [
-    trigger('popUp', [
-      state('true', style({ transform: 'translateY(0)'})),
-      transition('void => *', [
-        style({ transform: 'translateY(-20px)',
-                opacity: '0', }),
-        animate('500ms ease-out')
-      ])
-    ])
-  ]
-
+  styleUrls: ['./page.component.scss']
 })
+
 export class PageComponent implements OnInit {
 
   constructor() { }
